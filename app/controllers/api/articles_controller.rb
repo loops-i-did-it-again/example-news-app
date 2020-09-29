54308af848564ec61d0787c8c821e1673f0855a1
dani <dani.zaghian@gmail.com>
2020-09-29 10:03:37 -0700

    adds news api index

--- app/controllers/api/articles_controller.rb
+++ app/controllers/api/articles_controller.rb
@@ -1,2 +1,8 @@
 class Api::ArticlesController < ApplicationController
+
+  def index
+    @articles = HTTP.headers("X-Api-Key" => Rails.application.credentials.news_api[:api_key]).get("https://newsapi.org/v2/everything?q=#{params[:search]}").parse
+    render json: @articles
+  end
+
 end
